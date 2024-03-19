78f15c98c8847e11c7de66f84b4d92addd84fe53
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-19 16:07:25 +0530

    Create Discussion Form

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
         setisSidebar(prev => !prev);
     }
   return (
-    <div className="w-full h-full flex ">
-      <div className={`sidebar w-[25%] bg-[#1e375f] h-[100vh] ${isSidebar && 'w-[0%] duration-150'} duration-150`} >
+    <div className="w-full h-full flex max-md:flex-col">
+      <div className={`sidebar w-[25%] max-md:w-[70%] max-md:fixed z-50 bg-[#1e375f] h-[100vh] ${isSidebar && 'w-[0] duration-150'} duration-150`} >
         <div className='mt-7 flex justify-between px-5 items-center'>
             <Image className='rounded-full' src={"/user-acc.jpg"} alt="" height={40} width={40}/>
             <h2 className='font-bold text-2xl absolute ml-12 text-white'>Hello, User</h2>
@@ -34,12 +34,43 @@ const Hero = () => {
             <h5 className='text-[17px] text-white cursor-pointer font-semibold '>News/Interview</h5>
         </div>
       </div>
-      <div className={`discussionSection w-[50%] bg-green-500 h-[100vh] relative duration-150 ${isSidebar && 'w-[60%] duration-150'}`}>
+      <div className={`discussionSection w-[50%] max-md:w-[100%] bg-white h-[100vh] relative duration-150 px-4 ${isSidebar && 'w-[60%] duration-150'}`}>
         <div className='sidebarArrow absolute top-[44%] left-0 bg-[#1e375f] w-4 h-20 cursor-pointer' onClick={sidebar}>
         <Image className={`rounded-md rotate-90 duration-150 mt-6 ${isSidebar && '-rotate-90 duration-150'}`} src={"/downarrow.png"} alt="downarrow" height={40} width={40}/>
         </div>
+        <h2 className='text-red-300 text-4xl font-bold bg-slate-300 w-[50%] p-2 items-center text-center ml-4 mt-4 shadow-lg'>Discussion Forum</h2>
+        <div className='mt-3 discussionContainer flex flex-col gap-4'>
+        <div className={`discussionBlock bg-white py-4 rounded-md shadow-lg w-[100%] overflow-hidden`}>
+          <div className='userDetails flex items-center gap-6'>
+          <Image className={`rounded-md duration-150 ml-2 mt-2`} src={"/user-acc.jpg"} alt="downarrow" height={40} width={40}/>
+          <h3>Lorem Ipsum</h3>
+          <p className={`bg-blue-500 rounded-xl px-3 text-sm text-white `}>Sector 2</p>
+          <p className={`text-blue-500  ${isSidebar ? 'ml-[50%]' : 'ml-[50%]'}`}>2 mins ago</p>
+          </div>
+          <p className='commentSection mx-14 w-[70%] text-wrap  '>asdasdasdasdasdasda sdasddddddddddddddddddd dddddddddd ddddddddddddd dddassssssss ssssssssssssssssssssss ssssssssssssss</p>
+          <div className='shareLike flex items-center px-8'>
+            <div className='px-10 mt-3 gap-2 flex items-center'>
+            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/heart.png"} alt="downarrow" height={20} width={20}/>
+            <p>Likes</p>
+            </div>
+            <div className='px-10 mt-3 gap-2 flex items-center'>
+            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/eye.jpg"} alt="downarrow" height={20} width={20}/>
+            <p>2k</p>
+            </div>
+            <div className='px-10 mt-3 gap-2 flex items-center'>
+            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/comment.jpg"} alt="downarrow" height={20} width={20}/>
+            <p>2k comments</p>
+            </div>
+            <div className='px-10 mt-3 gap-2 flex items-center'>
+            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/share.png"} alt="downarrow" height={20} width={20}/>
+            <p>Shares</p>
+            </div>
+          </div>
+        </div>
+        
       </div>
-      <div className={`marketStoriesSection w-[25%] bg-yellow-500 h-[100vh] duration-150 ${isSidebar && 'w-[40%] duration-150'}`}>
+      </div>
+      <div className={`marketStoriesSection w-[25%] max-md:w-[100%] bg-yellow-500 h-[100vh] duration-150 ${isSidebar && 'w-[40%] duration-150'}`}>
         Market Stories
       </div>
     </div>
