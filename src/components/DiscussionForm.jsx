a28c5c8ad7d64b06c596fa07a8580f935c1028ae
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-20 21:09:05 +0530

    Seed Discussion data

--- src/components/DiscussionForm.jsx
+++ src/components/DiscussionForm.jsx
@@ -1,3 +1,4 @@
+import { discussion } from '@/constants'
 import Image from 'next/image'
 import React from 'react'
 
@@ -8,36 +9,38 @@ const DiscussionForm = ({isSidebar,sidebar}) => {
         <Image className={`rounded-md -rotate-90 duration-150 mt-6  ${isSidebar && 'rotate-90 duration-150'}`} src={"/downarrow.png"} alt="downarrow" height={40} width={40}/>
         </div>
         <h2 className='text-red-300 text-4xl font-bold bg-slate-300 max-md:hidden w-[50%] max-md:w-[80%] p-2 items-center text-center ml-4 mt-4 shadow-lg'>Discussion Forum</h2>
-        <div className='mt-3 discussionContainer flex flex-col gap-16'>
+        {discussion.map((items) => (
+        <div key={items.name} className='mt-3 discussionContainer flex flex-col gap-16'>
         <div className={`discussionBlock bg-white py-4 rounded-md shadow-lg w-[100%] overflow-hidden `}>
           <div className='userDetails flex items-center gap-6'>
-          <Image className={`rounded-md duration-150 ml-2 mt-2`} src={"/user-acc.jpg"} alt="downarrow" height={40} width={40}/>
-          <h3 className='max-md:text-sm max-sm:text-[14px]'>Lorem Ipsum</h3>
-          <p className={`bg-blue-500 rounded-xl px-3 text-sm text-white max-md:text-sm max-sm:text-[10px] `}>Sector 2</p>
-          <p className={`text-blue-500  ${isSidebar ? 'ml-[40%]' : 'ml-[50%]'} max-md:ml-[40%] max-sm:ml-[10%] max-sm:text-[12px]`}>2 mins ago</p>
+          <Image className={`rounded-md duration-150 ml-2 mt-2`} src={items.userUrl} alt="downarrow" height={40} width={40}/>
+          <h3 className='max-md:text-sm max-sm:text-[12px]'>{items.userName}</h3>
+          <p className={`bg-blue-500 rounded-xl px-3 text-sm text-white max-md:text-sm max-sm:text-[10px] `}>{items.type}</p>
+          <p className={`text-blue-500  ${isSidebar ? 'ml-[40%]' : 'ml-[50%]'} max-md:ml-[40%] max-sm:ml-[10%] max-sm:text-[12px]`}>{items.time}</p>
           </div>
-          <p className='commentSection mx-14 w-[70%] text-wrap max-md:text-sm  '>asdasdasdasdasdasda sdasddddddddddddddddddd </p>
+          <p className='commentSection mx-14 w-[70%] text-wrap max-md:text-sm  '>{items.post}</p>
           <div className='shareLike flex items-center gap-14 ml-16 max-md:text-[10px] max-md:gap-5'>
             <div className=' mt-3 gap-2 max-md:gap-1   flex items-center'>
             <Image className={`cursor-pointer rounded-md duration-150 `} src={"/heart.png"} alt="downarrow" height={20} width={20}/>
-            <p>Likes</p>
+            <p>{items.likes}</p>
             </div>
             <div className=' mt-3 gap-2 max-md:gap-1  flex items-center'>
             <Image className={`cursor-pointer rounded-md duration-150 `} src={"/eye.jpg"} alt="downarrow" height={20} width={20}/>
-            <p>2k</p>
+            <p>{items.views}</p>
             </div>
             <div className=' mt-3 gap-2 max-md:gap-1  flex items-center'>
-            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/comment.jpg"} alt="downarrow" height={20} width={20}/>
-            <p>2k comments</p>
+            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/comment.jpg"} alt="downarrow" height={16} width={16}/>
+            <p>{items.comments}</p>
             </div>
             <div className=' mt-3 gap-2 max-md:gap-1  flex items-center'>
-            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/share.png"} alt="downarrow" height={20} width={20}/>
-            <p>Shares</p>
+            <Image className={`cursor-pointer rounded-md duration-150 `} src={"/share.png"} alt="downarrow" height={16} width={16}/>
+            <p>share</p>
             </div>
           </div>
         </div>
         
       </div>
+      ))}
       </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -8,3 +8,50 @@ export const links = [
         hash : "#marketStories"
 }   
 ]
+export const discussion = [
+    {
+        userUrl : "/user-acc.jpg",
+        userName : "Souraav Singh",
+        type: "sector 4",
+        time : "2 mins ago",
+        likes : "",
+        views : "4k",
+        comments : "2k comments",
+        post : "Hello this is sourav, This is a Great Website for me , its user interface is very clear. "
+
+} ,
+    {
+        userUrl : "/user-acc.jpg",
+        userName : "Ankush Verma",
+        type: "sector 2",
+        time : "2 mins ago",
+        likes : "",
+        views : "5k",
+        comments : "2k comments",
+        post : "Hello this is ankush, This is a Great Website for me , its user interface is very clear. "
+
+} ,
+    {
+        userUrl : "/user-acc.jpg",
+        userName : "Ratann Taataaa ",
+        type: "sector 3",
+        time : "2 mins ago",
+        likes : "",
+        views : "100k",
+        comments : "20k comments",
+        post : "Hello this is Ratan, This is a Great Website for me , its user interface is very clear. "
+
+} ,
+    {
+        userUrl : "/user-acc.jpg",
+        userName : "Mukes Ambani",
+        type: "sector 1",
+        time : "10 hrs ago",
+        likes : "",
+        views : "10k",
+        comments : "5k comments",
+        post : "Hello this is Mukesh, This is a Great Website for me , its user interface is very clear. "
+
+} ,
+
+]
